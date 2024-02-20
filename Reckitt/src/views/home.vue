8823e559014998f8955a8e333ccfec3fefd7ab6b
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2024-02-20 15:12:38 +0000

    Did nav stuff

--- Reckitt/src/views/home.vue
+++ Reckitt/src/views/home.vue
@@ -1,5 +1,16 @@
 <template>
 
+    <div class="relative">
+        <img class="" src="../../public/banner.png">
+        <div class="absolute inset-0 flex items-center justify-center text-white">
+            <p class="">Welcome to</p>
+        </div>
+        <div class="absolute inset-0 flex items-bottom justify-center text-white">
+            <p class="">RECKITT ONLINE STORE</p>
+        </div>
+        
+    </div>
+
 </template>
 <script setup>
 
